06a5263a7652f2ea25ee694a1b8cddc28ea51fb3
Makaratel <pmakaewskij@mail.ru>
2018-12-24 18:33:54 +0300

    На предзащиту

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
 var popupLetter = document.querySelector('.popup-letter');
-
 var formOpen = document.querySelector('.contacts__btn');
+var formClose = popupLetter.querySelector('.popup-letter__button');
+var popupMap = document.querySelector('.popup-map');
+var mapOpen = document.querySelector('.contacts__link');
+var mapClose = popupMap.querySelector('.popup-map__button');
+var form = document.querySelector('.popup-letter__form');
+var formName = form.querySelector('[name = surname]');
+
 formOpen.addEventListener('click', function(evt){
 	evt.preventDefault();
 	popupLetter.classList.add('popup--show');
 })
 
-var formClose = popupLetter.querySelector('.popup-letter__button');
 formClose.addEventListener('click', function(){
 	popupLetter.classList.remove('popup--show');
+	popupLetter.classList.remove('popup--error');
 })
 
-
-var popupMap = document.querySelector('.popup-map');
-
-var mapOpen = document.querySelector('.contacts__link');
 mapOpen.addEventListener('click', function(evt){
 	evt.preventDefault();
 	popupMap.classList.add('popup--show');
 })
 
-var mapClose = popupMap.querySelector('.popup-map__button');
 mapClose.addEventListener('click', function(){
 	popupMap.classList.remove('popup--show');
 })
+
+form.addEventListener('submit', function(evt){
+	if(formName.value.length < 3 || /\d/.test(formName.value)){
+		evt.preventDefault();
+		popupLetter.classList.add('popup--error');
+		formName.classList.add('invalid');
+	}
+})
+	
