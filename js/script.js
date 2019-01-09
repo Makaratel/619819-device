58f654d75d18062e349a3cdaeb1a8c45de3ea8a5
Makaratel <pmakaewskij@mail.ru>
2019-01-09 20:48:21 +0300

    На защиту

--- js/script.js
+++ js/script.js
@@ -1,36 +1,38 @@
-var popupLetter = document.querySelector('.popup-letter');
-var formOpen = document.querySelector('.contacts__btn');
-var formClose = popupLetter.querySelector('.popup-letter__button');
-var popupMap = document.querySelector('.popup-map');
-var mapOpen = document.querySelector('.contacts__link');
-var mapClose = popupMap.querySelector('.popup-map__button');
-var form = document.querySelector('.popup-letter__form');
-var formName = form.querySelector('[name = surname]');
+var popupLetter = document.querySelector(".popup-letter");
+var formOpen = document.querySelector(".contacts__btn");
+var formClose = popupLetter.querySelector(".popup-letter__button");
+var popupMap = document.querySelector(".popup-map");
+var mapOpen = document.querySelector(".contacts__link");
+var mapClose = popupMap.querySelector(".popup-map__button");
+var form = document.querySelector(".popup-letter__form");
+var formName = form.querySelector("[name = surname]");
 
-formOpen.addEventListener('click', function(evt){
+formOpen.addEventListener("click", function(evt){
 	evt.preventDefault();
-	popupLetter.classList.add('popup--show');
+	popupLetter.classList.add("popup--show");
 })
 
-formClose.addEventListener('click', function(){
-	popupLetter.classList.remove('popup--show');
-	popupLetter.classList.remove('popup--error');
+formClose.addEventListener("click", function(){
+	popupLetter.classList.remove("popup--show");
+	popupLetter.classList.remove("popup--error");
 })
 
-mapOpen.addEventListener('click', function(evt){
+mapOpen.addEventListener("click", function(evt){
 	evt.preventDefault();
-	popupMap.classList.add('popup--show');
+	popupMap.classList.add("popup--show");
 })
 
-mapClose.addEventListener('click', function(){
-	popupMap.classList.remove('popup--show');
+mapClose.addEventListener("click", function(){
+	popupMap.classList.remove("popup--show");
 })
 
-form.addEventListener('submit', function(evt){
+form.addEventListener("submit", function(evt){
 	if(formName.value.length < 3 || /\d/.test(formName.value)){
 		evt.preventDefault();
-		popupLetter.classList.add('popup--error');
-		formName.classList.add('invalid');
+		popupLetter.classList.remove("popup--error");
+		popupLetter.classList.add("popup--error");
+		formName.classList.add("invalid");
 	}
-})
+	
+});
 	
